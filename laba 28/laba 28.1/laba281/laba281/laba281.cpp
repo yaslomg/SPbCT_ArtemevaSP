36d0d2a9ab11c7a6f93be227481300e0ef878e8a
Sofia <91670915+yaslomg@users.noreply.github.com>
2021-11-01 22:49:05 +0300

    Add files via upload

---
+++ laba 28/laba 28.1/laba281/laba281/laba281.cpp
@@ -0,0 +1,36 @@
+﻿// laba281.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _WIN32_WINNT 0x0500
+#ifndef UNICODE
+#define UNICODE
+#endif
+#include <windows.h>
+#include <stdio.h>
+#include <sddl.h>
+#include <Aclapi.h>
+void main(void) {
+	PSID pSid;
+	PSECURITY_DESCRIPTOR pSD;
+	PACL pDACL;
+	LPTSTR StringSid = NULL;
+	ULONG Error;
+	HANDLE hFile;
+	hFile = CreateFile(TEXT("MyFile.txt"), READ_CONTROL, 0, NULL, OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL, NULL);
+	Error = GetSecurityInfo(hFile,
+		SE_FILE_OBJECT, // тип объекта
+		OWNER_SECURITY_INFORMATION |
+		DACL_SECURITY_INFORMATION,
+		&pSid, // Sid владельца
+		NULL, // Sid группы
+		&pDACL, NULL, // списки прав доступа
+		&pSD); // дескриптор защиты;
+	if (Error != ERROR_SUCCESS)
+		printf("GetSecurityInfo Error\n");
+	if (!ConvertSidToStringSid(pSid, &StringSid)) /* память для строки выделяет сама функция */
+		printf("Convert SID to string SID failed.");
+	wprintf(L"StringSid %s\n", StringSid);
+	LocalFree(pSD);
+	LocalFree(StringSid);
+}
